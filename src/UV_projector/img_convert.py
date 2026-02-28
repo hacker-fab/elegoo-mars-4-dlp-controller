98174ece79b7d73c782bd1174f514b1dd6d30cf8
Edward Wang <edawordwang@gmail.com>
2026-02-28 02:43:55 -0500

    img_convert check

--- src/UV_projector/img_convert.py
+++ src/UV_projector/img_convert.py
@@ -3,10 +3,12 @@ import numpy as np
 
 
 def image_to_arr(path):
-    # TODO: run checks on image bit depth and so on
-
     print("-- Converting image to bytes --")
     im_frame = Image.open(path)
+    # convert to 8bit grayscale
+    if im_frame.mode != 'L':
+          im_frame = im_frame.convert('L')
     pixeldata = np.transpose(np.array(im_frame))
+    assert pixeldata.dtype == np.uint8, f"Expected uint8, got: {pixeldata.dtype}"
 
     return pixeldata
